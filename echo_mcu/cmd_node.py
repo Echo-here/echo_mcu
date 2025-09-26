b9a08f5398b5517f4fea76fe51d3a7baf7fe7204
bitbyte08 <me@bitworkspace.kr>
2025-09-26 11:29:47 +0900

    fix :: cmd node의 명령 조건 수정

--- echo_mcu/cmd_node.py
+++ echo_mcu/cmd_node.py
@@ -7,13 +7,37 @@ from std_msgs.msg import String
 class CmdNode(Node):
     def __init__(self):
         super().__init__('cmd_node')
-
         self.cmd_sub = self.create_subscription(Twist, '/cmd_vel', self.cmd_callback, 10)
         self.tx_pub = self.create_publisher(String, '/mcu_tx', 10)
 
     def cmd_callback(self, msg: Twist):
-        data = f"{msg.linear.x},{msg.linear.y},{msg.angular.z}"
-        self.tx_pub.publish(String(data=data))
+        duty = int(min(max(abs(msg.linear.x), abs(msg.angular.z)) * 10, 9))
+
+        if msg.linear.x == 0.0 and msg.angular.z == 0.0:
+            data = "s"
+            self.tx_pub.publish(String(data=data))
+            self.get_logger().info(f"Publish STOP: {data}")
+            return
+
+        if msg.linear.x > 0:
+            l_dir, r_dir = 'f', 'f'
+        elif msg.linear.x < 0:
+            l_dir, r_dir = 'b', 'b'
+        else:
+            l_dir, r_dir = 's', 's'
+
+        if msg.angular.z > 0: 
+            l_dir, r_dir = 'b', 'f'
+        elif msg.angular.z < 0:
+            l_dir, r_dir = 'f', 'b'
+
+        left_cmd = f"l {l_dir} {duty}"
+        right_cmd = f"r {r_dir} {duty}"
+
+        self.tx_pub.publish(String(data=left_cmd))
+        self.tx_pub.publish(String(data=right_cmd))
+
+        self.get_logger().info(f"Publish to MCU: {left_cmd}, {right_cmd}")
 
 def main(args=None):
     rclpy.init(args=args)
